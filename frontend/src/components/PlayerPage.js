31eadf0641d41c56fa60a247077a44f6de6e748b
TrevorKvanvig <tkvanvig10@gmail.com>
2023-08-06 14:19:08 -0700

    Recieve Stats Data Based on Player

--- frontend/src/components/PlayerPage.js
+++ frontend/src/components/PlayerPage.js
@@ -2,9 +2,15 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 const PlayerPage = () => {
   const {id} = useParams();
-  
-  useEffect(() => {
 
+  useEffect(() => {
+    const getAllStats = async () => {
+      const response = await fetch('https://www.balldontlie.io/api/v1/stats?player_ids[]=' + id)
+
+      const stats  = await response.json()
+      console.log(stats);
+    }
+    getAllStats()
   }, [id])
 
   return(<h1>Player Page for id {id}</h1>);
